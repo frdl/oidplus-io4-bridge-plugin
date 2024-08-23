9c8a987bce4bdfc55a67e24de987fc56e40cf2da
Till Wehowski <github@webfan.de>
2024-08-23 12:23:46 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@
  * Author URI: https://frdl.de
  * License: MIT
  */
-namespace Frdlweb\OIDplus;
+namespace Frdlweb\OIDplus\IO4\plugin;
 
 	use ViaThinkSoft\OIDplus\Core\OIDplus;
 	use ViaThinkSoft\OIDplus\Core\OIDplusConfig;
@@ -17,8 +17,61 @@ namespace Frdlweb\OIDplus;
 	use ViaThinkSoft\OIDplus\Core\OIDplusPagePluginRa;
 	use ViaThinkSoft\OIDplus\Core\OIDplusPlugin;
  
- 
 
+ function base64_url_encode($input) {
+   return strtr(base64_encode($input), '+/=', '~_-');
+ }
+
+ function base64_url_decode($input) {
+   return base64_decode(strtr($input, '~_-', '+/='));
+ }	
+
+
+ function isBase64Encoded($str)
+	{
+		try
+
+		{
+		$decoded = base64_decode($str, true);
+
+		if ( base64_encode($decoded) === $str ) {
+		    return true;
+		}
+		else {
+		    return false;
+		}
+
+		}catch(\Exception $e){
+			// If exception is caught, then it is not a base64 encoded string
+			return false;
+		}
+ }
+
+ function markdown($atts) {
+	 if(!isset($atts['content'])){
+		throw new \Exception('Content attribute must be set in '.__FUNCTION__); 
+	 }
+    // Extract shortcode attributes
+    $atts = shortcode_atts(
+        array(
+            'content'=>isBase64Encoded($atts['content']) ? base64_decode($atts['content']) : $atts['content'],
+        ),
+        $atts
+    );
+       $atts['content'] = isBase64Encoded($atts['content']) ? base64_decode($atts['content']) : $atts['content'];
+	 
+	 
+	     $frontMatter = new \Webuni\FrontMatter\FrontMatter();
+
+          $document = $frontMatter->parse($atts['content'] );
+
+           $data = $document->getData();
+           $content = $document->getContent();
+	 
+	 
+ 
+    return $content;
+}
 
 // header("Refresh:5; url=?goto=oidplus:system");
  function refresh_headér_shortcode($atts) {
@@ -39,10 +92,11 @@ namespace Frdlweb\OIDplus;
 }
 
 // Shortcode
+//add_shortcode('markdown', __NAMESPACE__.'\markdown');
 add_shortcode('RefreshHeader', __NAMESPACE__.'\refresh_headér_shortcode');
 add_shortcode('ListAllShortcodes', '\display_shortcodes');
 
-//you can use autowiring!!!
+//you can use autowiring as from container->invoker !!!
 return (function($container){
 	//print_r(get_class($container));
 });
