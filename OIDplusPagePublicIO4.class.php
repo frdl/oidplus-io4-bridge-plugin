369220e99a68b133da5c849af6cebdad21e10ae6
Till Wehowski <github@webfan.de>
2024-09-02 11:19:40 +0200

    Update OIDplusPagePublicIO4.class.php

--- OIDplusPagePublicIO4.class.php
+++ OIDplusPagePublicIO4.class.php
@@ -381,7 +381,7 @@ ORDER BY (data_length + index_length) DESC";
     $pow = min($pow, count($units) - 1); 
    
     // Uncomment one of the following alternatives
-    // $bytes /= pow(1024, $pow);
+      $bytes /= pow(1024, $pow);
     // $bytes /= (1 << (10 * $pow)); 
    
     return round($bytes, $precision) . $units[$pow]; 
